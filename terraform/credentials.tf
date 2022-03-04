a848e9a7cc8a40ba8d6a616bb27c4b44214e398e
Juanma Orgaz <juanma.orgaz@gmail.com>
2022-03-04 12:18:04 +0100

    Initial estructure1

--- terraform/credentials.tf
+++ terraform/credentials.tf
@@ -1,6 +1,6 @@
 provider "azurerm" {
   features {}
-  subscription_id = "<SUBSCRIPCION ID>"
+  subscription_id = ""
   client_id       = "<APP_ID>"
   client_secret   = "<PASSWORD>"
   tenant_id       = "<TENANT>"
