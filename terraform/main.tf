5743ec965daa4f7228bcc42bef60faaaf263bd2f
Juanma Orgaz <juanma.orgaz@gmail.com>
2022-03-04 13:15:54 +0100

    Update vars.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,4 @@
+# Provider
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
 
 terraform {
@@ -9,6 +10,7 @@ terraform {
   }
 }
 
+# Resource Group
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
 
 resource "azurerm_resource_group" "rg" {
@@ -34,5 +36,30 @@ resource "azurerm_storage_account" "stAccount" {
     tags = {
         environment = "UNIR CP2"
     }
+
+}
+
+# Create Network
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_network
+
+resource "azurerm_virtual_network" "myNet" {
+    name                = "kubernetesnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+    tags = {
+        environment = "CP2"
+    }
+}
+
+# Create Subnet
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/subnet
+
+resource "azurerm_subnet" "mySubnet" {
+    name                   = "terraformsubnet"
+    resource_group_name    = azurerm_resource_group.rg.name
+    virtual_network_name   = azurerm_virtual_network.myNet.name
+    address_prefixes       = ["10.0.1.0/24"]
 
 }
