1caefb7068bbad49695abc05e1fe30f2b5648f70
Juanma Orgaz <juanma.orgaz@gmail.com>
2022-03-14 16:01:29 +0100

    app

--- terraform/master.tf
+++ terraform/master.tf
@@ -70,6 +70,18 @@ resource "azurerm_network_security_group" "masterSecGroup" {
         destination_address_prefix = "*"
     }
 
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1003
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "30080"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
     tags = {
         environment = "UNIR CP2"
         node        = "MASTER"
